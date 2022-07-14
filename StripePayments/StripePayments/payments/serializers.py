edd95b4e4740a01a85f06d696b58711041ebe16e
Yekaterina Mikhalchuk <83668755+yekaterinamikhalchuk@users.noreply.github.com>
2022-07-14 14:31:21 +0300

    Delete StripePayments directory

--- StripePayments/StripePayments/payments/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Item
-
-class ItemDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Item
-        fields='__all__'
